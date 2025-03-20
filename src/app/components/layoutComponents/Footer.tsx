3a13232e6f434a5a427a623b875a9e987ad659a0
BetsyArinze <betarinze@gmail.com>
2025-03-20 22:36:38 +0100

    feat: add footer

--- src/app/components/layoutComponents/Footer.tsx
+++ src/app/components/layoutComponents/Footer.tsx
@@ -1,8 +1,51 @@
+import { navlinks } from "@/app/utils/navlinks";
+import Link from "next/link";
+import Paragraph from "../ui/Paragraph";
+import { socialLinks } from "@/app/utils/socialLinks";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <>
-      <footer className="mt-auto">Footer</footer>
-    </>
+    <section className="px-4 pb-4">
+      <footer className="flex mt-auto bg-white  rounded-b-md w-full items-center justify-center py-12 md:py-16 lg:py-24 px-8">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <ul className="flex gap-4 pb-4">
+            {navlinks
+              .filter((link) => link.title !== "Home")
+              .map((link) => {
+                return (
+                  <li key={link.title} className="flex gap-8">
+                    <Link href={link.path} className="text-neutral-600">
+                      {link.title}
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+          <ul className="flex gap-6">
+            {socialLinks.map((social) => {
+              return (
+                <li key={social.name}>
+                  <Link href={social.link}>
+                    <Image
+                      src={social.icon}
+                      alt={social.name}
+                      height={24}
+                      width={24}
+                    />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <div>
+            <Paragraph className="text-center">
+              © 2024 Abstractly, Inc. All rights reserved.
+            </Paragraph>
+          </div>
+        </div>
+      </footer>
+    </section>
   );
 };
 
---
+++ src/app/utils/socialLinks.ts
@@ -0,0 +1,27 @@
+export const socialLinks = [
+  {
+    name: "YouTube",
+    icon: "/social/youtube.svg",
+    link: "",
+  },
+  {
+    name: "Instagram",
+    icon: "/social/instagram.svg",
+    link: "",
+  },
+  {
+    name: "Facebook",
+    icon: "/social/facebook.svg",
+    link: "",
+  },
+  {
+    name: "Github",
+    icon: "/social/github.svg",
+    link: "",
+  },
+  {
+    name: "X",
+    icon: "/social/x.svg",
+    link: "",
+  },
+];
