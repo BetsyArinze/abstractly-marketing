a143a763d2118833d27c75af40e750577a187831
BetsyArinze <betarinze@gmail.com>
2025-04-08 22:32:17 +0100

    chore: Optimize SEO

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,17 @@ import dynamic from "next/dynamic";
 import ContactUs from "../contact/components/ContactSection";
 import AboutUsHero from "./components/AboutUsHero";
 import Statistics from "./components/Statistics";
+import { Metadata } from "next";
+
+const PAGE_TITLE = "About us | Abstractly";
+export const metadata: Metadata = {
+  title: PAGE_TITLE,
+  description: "Know more about us at Abstractly",
+  openGraph: {
+    title: PAGE_TITLE,
+    description: "Know more about us at Abstractly",
+  },
+};
 
 const About = () => {
   const Team = dynamic(() => import("./components/Team"), {
